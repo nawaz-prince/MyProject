de6275b6aedbbd3da069e5338097598c5c50d88c
Nawaz Prince <157207924+nawaz-prince@users.noreply.github.com>
2024-05-15 00:21:28 +0530

    app.py

--- app.py
+++ app.py
@@ -24,10 +24,10 @@ def get_stock_price_fig(df):
 
 
 def get_more(df):
-    df['EWA_20'] = df['Close'].ewm(span=20, adjust=False).mean()
+    df['EMA_20'] = df['Close'].ewm(span=20, adjust=False).mean()
     fig = px.scatter(df,
                      x="Date",
-                     y="EWA_20",
+                     y="EMA_20",
                      title="Exponential Moving Average vs Date")
     fig.update_traces(mode='lines+markers')
     return fig
